b9f1a661697f8d00986dbf47760ea5ce0e7d27dc
Vincent <vfung26@my.bcit.ca>
2024-11-22 21:49:54 -0800

    back to pc

--- TermProject/src/ca/bcit/comp2522/TermProject/Score.java
+++ TermProject/src/ca/bcit/comp2522/TermProject/Score.java
@@ -1,13 +1,19 @@
 package ca.bcit.comp2522.TermProject;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Score
 {
     private final LocalDateTime currentTime;
     private final DateTimeFormatter formatter;
     private final String formattedDateTime;
+    private final List<String> scoreList;
 
     private static final String OUTPUT_DIRECTORY = "src";
     private static final String OUTPUT_FILE = "src/score.txt";
@@ -25,6 +31,7 @@ public class Score
         currentTime = LocalDateTime.now();
         formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         formattedDateTime = currentTime.format(formatter);
+        scoreList = new ArrayList<>();
 
         this.numGamesPlayed = numGamesPlayed;
         this.numCorrectFirstAttempt = numCorrectFirstAttempt;
@@ -32,11 +39,11 @@ public class Score
         this.numIncorrectTwoAttempts = numIncorrectTwoAttempts;
     }
 
-    /**
+    /*
      * Getter function for the formattedDateTime
      * @return the formatted date time as a String
      */
-    public String getFormattedDateTime()
+    private String getFormattedDateTime()
     {
         return formattedDateTime;
     }
@@ -45,5 +52,14 @@ public class Score
     {
         Score s = new Score(3, 3, 3, 3);
         System.out.println(s.getFormattedDateTime());
+    }
+
+    /*
+     * Writes the score to the file.
+     * @param outputFilePath
+     */
+    private void writeToScoreFile(final File outputFilePath)
+    {
+        Files.write(outputFilePath, scoreList, StandardOpenOption.APPEND);
     }
 }
